5d07c30463245b2e264cbb64c97095067f66b021
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2023-06-28 23:21:11 +0530

    database connectivity(PostgreSQL)

--- Store/models.py
+++ Store/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now_add=True)
 
 
-class Customer(models.model):
+class Customer(models.Model):
     first_name = models.CharField(max_length=225)
     last_name = models.CharField(max_length=225)
     email = models.EmailField(unique=True)
@@ -32,7 +32,7 @@ class Order(models.Model):
     ]
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_SATTUS_PENDING)
+    payment_status = models.CharField(max_length=225, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_SATTUS_PENDING)
 
 
 class OrderItem(models.Model):
@@ -46,7 +46,7 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    
+
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
