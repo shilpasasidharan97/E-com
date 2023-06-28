6bd54cb771dc2170d1f1dd8aefe431c72e540935
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2023-06-28 22:49:44 +0530

    order and cart model

--- Store/models.py
+++ Store/models.py
@@ -20,4 +20,33 @@ class Customer(models.model):
     phone = models.CharField(max_length=12)
     birth_date = models.DateField(null=True)
 
+
+class Order(models.Model):
+    PAYMENT_SATTUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        ('PAYMENT_SATTUS_PENDING','Pending'),
+        ('PAYMENT_STATUS_COMPLETE','Complete'),
+        ('PAYMENT_STATUS_FAILED','Failed')
+    ]
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_SATTUS_PENDING)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    carted_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
     
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
