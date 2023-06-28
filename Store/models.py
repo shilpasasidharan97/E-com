027ce85c81c544836a06b142547d5dd16f5271dc
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2023-06-28 22:33:28 +0530

    product and customer model creation

--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Collection(models.Model):
+    titile = models.CharField(max_length=225)
+
+
+class Product(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    title = models.CharField(max_length=225)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now_add=True)
+
+
+class Customer(models.model):
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=12)
+    birth_date = models.DateField(null=True)
+
+    
